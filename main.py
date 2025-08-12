cf76c1fef03dd95782965fe46ec07773bdb3cc73
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2025-08-11 21:55:52 -0700

    main.py get_num_words(text)

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
     
+def get_num_words(text):
+    num_words = len(text.split())
+    return f"{num_words} words found in the document"
+    
+    
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    print(get_num_words(text))
 
 main()
 
