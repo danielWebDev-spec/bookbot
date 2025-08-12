1ced4a78cc63baee0f87f27724005e8cc7500073
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2025-08-12 02:42:00 -0700

    stats.py: f'{num_words} words found in the document'

--- stats.py
+++ stats.py
@@ -1,9 +1,6 @@
 def get_num_words(text):
     num_words = len(text.split())
-    return f"{num_words} words found in the document"
-
-def sort_on(items):
-    return items
+    return f"Found {num_words} total words"
 
 def count_char_occurrences(book):
     # start with empty dictionary
